883ec5a40f970143dd45bb268dd1c839d65c1032
Andre Garcia <andreemmanuelgarcia@gmail.com>
2022-10-11 12:36:53 +0800

    Added functionality up to 99
    Modified function to work up to the number 99

--- 01-numToWord/index.js
+++ 01-numToWord/index.js
@@ -1,28 +1,36 @@
 /**
  * Create a program that converts a number input to words
  * `function numToWord(n: number): string {}`
- * input n is 1 to 9
+ * input n is 1 to 99
  *
  * coordinate with repository admin for further instructions
+*/
+
+const digitNames = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+const tenToNineteen = ["Ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", 
+      "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+const tensDigits = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+/**
+ * This function returns the word equivalent of numbers between 1 to 99
+ * @param {n} A Number between 1 to 9
+ * @returns The name of the number
  */
+function numToWord(n) {
+  let word = "";
 
- const digitNames = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+  if(n < 10)
+    word += digitNames[n%10];
+  else if(n < 20)
+    word += tenToNineteen[n%10];
+  else  {
+    word += tensDigits[Math.floor((n/10)) - 2]
+    if(n%10 != 0)
+      word += "-" + digitNames[n%10];
+  }
+  
+  return word;
+}
 
- /**
-  * This function returns the word equivalent of numbers between 1 to 9
-  * @param {n} A Number between 1 to 9
-  * @returns The name of the number
-  */
- function numToWord(n) {
-   return digitNames[n-1];
- }
- 
- console.log(numToWord(1));
- console.log(numToWord(2));
- console.log(numToWord(3));
- console.log(numToWord(4));
- console.log(numToWord(5));
- console.log(numToWord(6));
- console.log(numToWord(7));
- console.log(numToWord(8));
- console.log(numToWord(9));
+for(let i = 0; i < 100; i++) {
+  console.log(numToWord(i));
+}
