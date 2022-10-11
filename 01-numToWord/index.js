b2d2fbea3b4a302cdd12debf478b58c5e628cbd4
Andre Garcia <andreemmanuelgarcia@gmail.com>
2022-10-11 12:04:08 +0800

    Revert "Accomplish Solution 2"
    This reverts commit e55aec271a0c2f93fa99bff60dde5be3f464195e.

--- 01-numToWord/index.js
+++ 01-numToWord/index.js
@@ -1,16 +1,13 @@
-// Create a program that return true if a year is a leap year, otherwise returns false
-// `function isLeapYear(n: number): boolean`
-
 /**
- * This function returns true if the year provided is a leap year, and false if it is not.
- * @param {n} Year
- * @returns A boolean value that refers to whether the year is a leapyear
+ * Create a program that converts a number input to words
+ * `function numToWord(n: number): string {}`
+ * input n is 1 to 9
+ *
+ * coordinate with repository admin for further instructions
  */
- function isLeapYear(n) {
-  return n % 4 == 0 && (n % 100 != 0 || n % 400 == 0);
+
+function numToWord(n) {
+  return n;
 }
 
-console.log(isLeapYear(1900));
-console.log(isLeapYear(1995));
-console.log(isLeapYear(2000));
-console.log(isLeapYear(2004));
+console.log(numToWord(1995));
