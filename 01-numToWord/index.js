f20d7ef9a3de43153693ffefcdd737ae2182737b
Andre Garcia <andreemmanuelgarcia@gmail.com>
2022-10-11 15:17:30 +0800

    Adjusted NumtoWord to word up to 9 digits

--- 01-numToWord/index.js
+++ 01-numToWord/index.js
@@ -9,28 +9,52 @@
 const digitNames = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
 const tenToNineteen = ["Ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", 
       "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
-const tensDigits = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+const tensDigits = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+const nextZeroes = ["","Thousand","Million"];
+
+
 /**
- * This function returns the word equivalent of numbers between 1 to 99
+ * This function returns the word equivalent of numbers between 1 to 999 it will be used if the numbers
+ * continue to scale for ease of code
+ * @param {n} A Number between 1 to 999
+ * @returns The name of the number
+ */
+function oneToNineHundredNinetyNine(n){
+  let word = "";
+
+  if(n >= 100 && n < 1000)
+    word += digitNames[Math.floor(n/100)] + " Hundred" + " ";
+
+  // eleven to nineteen
+  if(Math.floor(n % 100 / 10) == 1) {
+    word += tenToNineteen[n%10];
+  } 
+  else if (Math.floor(n % 100 / 10) > 1) {
+    word += tensDigits[Math.floor((n % 100 / 10)) - 2];
+    if(n%10 != 0)
+      word += "-" + digitNames[n % 10];
+  } else word += digitNames[n%10];
+  
+  return word;
+}
+
+/**
+ * This function returns the word equivalent of numbers between 1 to 9999
  * @param {n} A Number between 1 to 9
  * @returns The name of the number
  */
 function numToWord(n) {
   let word = "";
+  let numLength = n.toString().length;
 
-  if(n < 10)
-    word += digitNames[n%10];
-  else if(n < 20)
-    word += tenToNineteen[n%10];
-  else  {
-    word += tensDigits[Math.floor((n/10)) - 2]
-    if(n%10 != 0)
-      word += "-" + digitNames[n%10];
+  for(let i = numLength - 1; i > 3; i-=3) {
+    word += oneToNineHundredNinetyNine(Math.floor(n / Math.pow(1000,Math.floor(i/3)))) + " " + nextZeroes[Math.ceil(i/3) - 1]+ " ";
+    n = n % Math.pow(1000, Math.floor(i/3));
   }
   
+  word += oneToNineHundredNinetyNine(n);
+
   return word;
 }
 
-for(let i = 0; i < 100; i++) {
-  console.log(numToWord(i));
-}
+console.log(numToWord(102220812))
